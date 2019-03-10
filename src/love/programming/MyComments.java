a6d8312fd56f84c57d4b22ad4a739f228c32650b
jyothipaul@yahoo.com <jyothipaul@yahoo.com>
2019-03-10 18:29:41 +0000

    JavaCommentsProject

---
+++ src/love/programming/MyComments.java
@@ -0,0 +1,11 @@
+package love.programming;
+// just giving  the comment
+public class MyComments {
+
+    public static void main(String[] args) {
+        System.out.println();
+        //another comment
+    }
+}
+
+
